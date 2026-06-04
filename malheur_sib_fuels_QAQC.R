1af0c7f55ca057c65f04e6146cd6a749dae7a46b
esande02 <esande02@uw.edu>
2026-06-04 11:14:00 -0700

    Created Separate QAQC Scripts for Trees and Fuels Data

---
+++ malheur_sib_fuels_QAQC.R
@@ -0,0 +1,63 @@
+#############################################################################################
+# Malheur SIB Fuels QA/QC Script
+# Author: Emily Sanders, Jim Cronan
+# Purpose: Review Malheur SIB Fuels Data for errors
+# Date: June 4, 2026
+#############################################################################################
+
+#Load libraries
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(readxl)#readxl()
+library(tidyverse)
+
+#---------------------------------------------------------------------------------------------
+# 1. Load data
+#---------------------------------------------------------------------------------------------
+
+#Data
+#SIB Fuels Data
+user_paths_data <- c(
+  esande02 = 
+  mak600 = 
+  jcronan =
+)
+
+#Lookup Tables
+# Map usernames to file paths
+user_paths_lut <- c(
+  esande02 = 
+  mak600 = 
+  jcronan =
+)
+
+#Detect current user
+current_user <- Sys.info()[["user"]]
+
+# Check if user exists in mapping for data files
+if (!current_user %in% names(user_paths_data)) {
+  stop("No file path configured for this user: ", current_user)
+}
+
+# Check if user exists in mapping for lut files
+if (!current_user %in% names(user_paths_lut)) {
+  stop("No file path configured for this user: ", current_user)
+}
+
+# Load data
+dim <- read_excel(paste(user_paths_data[current_user], 
+                       "",
+                        sep = ""))
+
+cw <- read_excel(paste(user_paths_data[current_user], 
+                       "",
+                       sep = ""))
+# Load LUTs
+plot_lut <- read_excel(paste(user_paths_lut[current_user], 
+                             "",
+                             sep = ""))
+
+species_lut <- read.csv(paste(user_paths_lut[current_user], 
+                              "",
+                              sep = ""))
